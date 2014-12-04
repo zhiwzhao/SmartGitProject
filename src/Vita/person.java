0c0de18b17d4c8c70d12f55b6192d32039f6fad1
vitazhao <zhiwzhao@cisco.com>
2014-12-04 09:59:42 +0800

    this is first commit version

---
+++ src/Vita/person.java
@@ -0,0 +1,13 @@
+package Vita;
+
+public class person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public String getName() {
+		return "VitaZhao";		
+	}
+
+}
---
+++ src/Vita/testsmartgit.java
@@ -0,0 +1,13 @@
+package Vita;
+
+public class testsmartgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test project for smart git.");
+		person person=new person();
+		
+		System.out.println(person.getName());
+	}
+
+}
